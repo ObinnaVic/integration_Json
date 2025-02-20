86150d81be46a7e23052503077cdfadd9c1cae7d
victornkire <nkirevictor77@gmail.com>
2025-02-20 18:46:45 +0100

    Fix: cors adjustments

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ app.use(express.json());
 
 app.get("/integration.json", (req, res) => {
     return res.status(200).json({
-        data: Spec.data,
-        "status": "accepted"
+        data: Spec.data
     })
 })
 
