9052ebc336918ecb19c19986189835a756c8e932
victornkire <nkirevictor77@gmail.com>
2025-02-20 11:38:42 +0100

    Updating integration json

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.get("/integration.json", (req, res) => {
     return res.status(200).json({
-        data: Spec,
+        data: Spec.data,
         "status": "accepted"
     })
 })
